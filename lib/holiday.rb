ae0c732e65c10a9042e869ff8c73f0f8e6f0b60f
Ian Gutwinski <iangutwinski@gmail.com>
2019-02-03 23:26:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ holiday_hash.each do |season, holiday|
   puts season.to_s.capitalize!+":"
   holiday.each do |holiday, supplies|
 
-    puts holiday.to_s.capitalize+": "+supplies.flatten.to_s
+    puts holiday.to_s.capitalize+": "+supplies.to_s
 
   end
 
